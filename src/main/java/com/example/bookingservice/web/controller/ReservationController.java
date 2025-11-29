7052acbfe5363de1efc46b71d0cb35892ef02ba9
JekoGyulev <zhekogyulev@gmail.com>
2025-11-29 16:57:06 +0200

    feat: implement ReservationController that will handle requests , coming from the Main Project(Spring MVC - StayConnected)

---
+++ src/main/java/com/example/bookingservice/web/controller/ReservationController.java
@@ -0,0 +1,19 @@
+package com.example.bookingservice.web.controller;
+
+
+import com.example.bookingservice.reservation.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+}
